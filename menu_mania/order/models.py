eb836bfa4cb69759d70aed305d19e7bff18a317c
Ryan Stephens <ryanstephens@MacBook-Pro.local>
2020-11-29 23:11:32 -0600

    extend templates

--- menu_mania/order/models.py
+++ menu_mania/order/models.py
@@ -12,7 +12,5 @@ class Order(models.Model):
     )
     time_ordered = models.DateTimeField(auto_now=True)
     time_delivered = models.DateTimeField()
-    dish = models.ForeignKey(
-        "menu.Dish",
-        on_delete=models.CASCADE
-    )
+    dish = models.ManyToManyField("menu.Dish")
+    cost = models.DecimalField(max_digits=7, decimal_places=2, default=0)
